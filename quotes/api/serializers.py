b844db166127b01d5a9091f1fdf4a2cb0c4e0bfe
Michael Andrew <michael.andrew.hf@gmail.com>
2026-09-04 15:39:37 -0300

    fix: validate quote updates and equipment representation

--- quotes/api/serializers.py
+++ quotes/api/serializers.py
@@ -54,7 +54,7 @@ class GeneratedDocumentSerializer(serializers.ModelSerializer):
 
 class QuoteSerializer(serializers.ModelSerializer):
     customer_name = serializers.CharField(source="customer.name", read_only=True)
-    equipment_label = serializers.CharField(source="equipment.__str__", read_only=True)
+    equipment_label = serializers.SerializerMethodField()
     work_order_number = serializers.IntegerField(source="work_order.number", read_only=True)
     display_number = serializers.CharField(read_only=True)
     items_total = serializers.DecimalField(max_digits=14, decimal_places=2, read_only=True)
@@ -102,6 +102,30 @@ class QuoteSerializer(serializers.ModelSerializer):
             "updated_at",
         ]
 
+    def get_equipment_label(self, obj):
+        return str(obj.equipment) if obj.equipment_id else None
+
+    def validate(self, attrs):
+        instance = self.instance
+        if instance is None:
+            return attrs
+        customer = attrs.get("customer", instance.customer)
+        equipment = attrs.get("equipment", instance.equipment)
+        work_order = attrs.get("work_order", instance.work_order)
+        if customer.deleted_at:
+            raise serializers.ValidationError({"customer": "Cliente excluido nao pode ser utilizado."})
+        if equipment:
+            if equipment.deleted_at:
+                raise serializers.ValidationError({"equipment": "Equipamento excluido nao pode ser utilizado."})
+            if equipment.customer_id != customer.id:
+                raise serializers.ValidationError({"equipment": "O equipamento deve pertencer ao cliente."})
+        if work_order:
+            if work_order.customer_id != customer.id:
+                raise serializers.ValidationError({"work_order": "A OS deve pertencer ao cliente."})
+            if equipment and work_order.equipment_id != equipment.id:
+                raise serializers.ValidationError({"work_order": "A OS deve pertencer ao equipamento selecionado."})
+        return attrs
+
 
 class QuoteCreateSerializer(serializers.Serializer):
     customer = serializers.PrimaryKeyRelatedField(queryset=Customer.objects.filter(deleted_at__isnull=True))
@@ -139,6 +163,17 @@ class QuoteItemCreateSerializer(serializers.Serializer):
     unit_price = serializers.DecimalField(max_digits=12, decimal_places=2)
     discount = serializers.DecimalField(max_digits=12, decimal_places=2, required=False)
     sort_order = serializers.IntegerField(required=False, min_value=0)
+
+    def validate(self, attrs):
+        item_type = attrs["item_type"]
+        if item_type == QuoteItemType.SERVICE and not attrs.get("service_type"):
+            raise serializers.ValidationError({"service_type": "Selecione o tipo de servico."})
+        if item_type == QuoteItemType.PART and not attrs.get("part"):
+            raise serializers.ValidationError({"part": "Selecione a peca."})
+        subtotal = attrs["quantity"] * attrs["unit_price"]
+        if attrs.get("discount", 0) > subtotal:
+            raise serializers.ValidationError({"discount": "Desconto nao pode superar o subtotal do item."})
+        return attrs
 
 
 class QuoteTerminalSerializer(serializers.Serializer):
