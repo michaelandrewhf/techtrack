c07163801381c7cb735c4cc5bb7f225fdc78b474
Michael Andrew <michael.andrew.hf@gmail.com>
2026-09-04 21:27:23 -0300

    feat: configure email and reset timeout

--- config/settings.py
+++ config/settings.py
@@ -159,3 +159,34 @@ SPECTACULAR_SETTINGS = {
         "DocumentTypeEnum": "quotes.models.DocumentType",
     },
 }
+
+EMAIL_BACKEND = os.environ.get(
+    "EMAIL_BACKEND",
+    (
+        "django.core.mail.backends.console.EmailBackend"
+        if DEBUG
+        else "django.core.mail.backends.smtp.EmailBackend"
+    ),
+)
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True").lower() in {
+    "1",
+    "true",
+    "yes",
+    "on",
+}
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "False").lower() in {
+    "1",
+    "true",
+    "yes",
+    "on",
+}
+DEFAULT_FROM_EMAIL = os.environ.get(
+    "DEFAULT_FROM_EMAIL",
+    "TechTrack <noreply@techtrack.local>",
+)
+FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:5173")
+PASSWORD_RESET_TIMEOUT = int(os.environ.get("PASSWORD_RESET_TIMEOUT", "3600"))
