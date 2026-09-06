a107d1548a3394c3dd14d37992657e02ad6c1c81
Michael Andrew <michael.andrew.hf@gmail.com>
2026-09-06 10:53:34 -0300

    fix: avoid dependency sync during backend startup

--- docker/backend-entrypoint.sh
+++ docker/backend-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 
-uv run --frozen python manage.py migrate --noinput
-exec uv run --frozen python manage.py runserver 0.0.0.0:8000
+python manage.py migrate --noinput
+exec python manage.py runserver 0.0.0.0:8000
